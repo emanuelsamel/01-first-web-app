77d1f67640a32259a48a820331ed23e62c89e121
emanuelsamel <mannys94@hotmail.com>
2017-06-01 16:56:31 -0400

    final commit

--- server.rb
+++ server.rb
@@ -7,3 +7,22 @@ end
 get "/portfolio" do
  erb :gallery
 end
+
+get "/about_me" do
+  @skills = ["git", "HTML", "CSS", "RUBY"]
+  @interests = ["cars", "animals", "food"]
+erb :me
+end
+
+get "/favourites" do
+  @fav_links = ["https://github.com/emanuelsamel/cmr/blob/master/contact.rb", "https://github.com/emanuelsamel/cmr/blob/master/contact.rb"]
+erb :favourites
+end
+
+get "/" do
+  redirect to "/home"
+end
+
+get "/gallery" do
+  redirect to"/favourites"
+end
